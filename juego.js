dc888aae5cc8eff89ad3c2dac4d74e3d58c1011b
noeliatilioanstasi <abigailnoelia44@gmail.com>
2024-11-15 10:57:56 -0300

    Add files via upload

---
+++ juego.js
@@ -0,0 +1,10 @@
+const preguntas = [
+    { pregunta: "¿Quién es el autor de 'Cien años de soledad'?", opciones: ["Gabriel García Márquez", "Mario Vargas Llosa", "Julio Cortázar"], respuesta: "Gabriel García Márquez" },
+    { pregunta: "¿Cuál es la capital de Francia?", opciones: ["Madrid", "Roma", "París"], respuesta: "París" },
+    { pregunta: "¿Qué es el ADN?", opciones: ["Ácido desoxirribonucleico", "Ácido ribonucleico", "Ácido nucleico"], respuesta: "Ácido desoxirribonucleico" },
+    { pregunta: "¿En qué año se cayó el Muro de Berlín?", opciones: ["1989", "1991", "1985"], respuesta: "1989" },
+    { pregunta: "¿Cuál es el río más largo del mundo?", opciones: ["Amazonas", "Nilo", "Yangtsé"], respuesta: "Amazonas" },
+    { pregunta: "¿Quién pintó la Mona Lisa?", opciones: ["Leonardo da Vinci", "Vincent van Gogh", "Pablo Picasso"], respuesta: "Leonardo da Vinci" },
+    { pregunta: "¿Qué significa la sigla 'ONU'?", opciones: ["Organización de las Naciones Unidas", "Operaciones Nacionales Unidas", "Órgano Nacional Unificado"], respuesta: "Organización de las Naciones Unidas" },
+    { pregunta: "¿En qué continente está el desierto de Sahara?", opciones: ["Asia
+  
